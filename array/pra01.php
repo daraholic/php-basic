5034d7f4554b6af85cbfffa1b5f818076006f845
annt <1112dara@gmail.com>
2021-10-22 14:28:37 +0800

    使用for迴圈產生亂數

--- array/pra01.php
+++ array/pra01.php
@@ -5,18 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>建立陣列練習</title>
-    <style>
-        h1 {
-            text-align:center;
-        }
-    </style>
+    <link rel="stylesheet" href="./styles.css">
+
 </head>
 <body>
     <h1>建立陣列練習</h1>
 <?php
 $name=['judy','amo','john','peter','hebe'];
 $subject=['國文','英文','數學','地理','歷史'];
-$name['judy']=[95,64,70,90,84];
+
+
+$name['judy']=['國文'=>95,64,70,90,84];
 $name['amo']=[95,64,70,90,84];
 $name['john']=[95,64,70,90,84];
 $name['peter']=[95,64,70,90,84];
@@ -29,6 +28,15 @@ print_r($subject);
 echo "</pre>";
 
 print_r( $name['judy']);
+
+$students=array_keys($name);
+echo "</pre>";
+print_r($students);
+
+$subject=arry_keys($name['judy']);
+echo "</pre>";
+print_r($subject);
+
 echo "judy的數學成績為:".$name['judy'][2];
 ?>
 
---
+++ array/pra02.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <h1>利用程式來產生陣列</h1>
+
+<?php
+$nine=[];
+
+// 指定變數要用固定植&明確結果
+for($j=1;$j<=9;$j++){
+    
+    for($i=1;$i<=9;$i++){
+        $nine[]="$j X $i =".($j*$i);
+    }
+}
+
+// echo "<pre>";
+// print_r($nine);
+// echo "</pre>";
+
+// 計算陣列個數函式 count()
+// 每九個換行
+// 解1
+for($i=0;$i<count($nine);$i++){
+    echo $nine[$i];
+    if($i%9==8){
+        echo "<br>";
+    }
+}
+// 解2
+for($i=0;$i<count($nine);$i++){
+    echo $nine[$i];
+    if(($i+1)%9==0){
+        echo "<br>";
+    }
+}
+
+?>
+
+</body>
+</html>
