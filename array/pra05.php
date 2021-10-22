e2c1265996f7d836816bf45318c4bf36919479a9
annt <1112dara@gmail.com>
2021-10-22 16:03:52 +0800

    簡化天干地支的寫法

--- array/pra05.php
+++ array/pra05.php
@@ -15,22 +15,14 @@
     $sky=['甲','乙','丙','丁','戊','己','庚','辛','壬','癸',];
     $land=['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥'];
     for($i=0;$i<60;$i++){
-        $zodiac[]=$sky[$i%10].$land[$i%12];
+        $zodiac[1984+$i]=$sky[$i%10].$land[$i%12];
     }
-    
+
     echo "<pre>";
     print_r($zodiac);
     echo "</pre>";
 
-    $tt=[];
-    for($i=0;$i<60;$i++){
-        $tt[1984+$i]=$zodiac[$i];
-    }
-    echo "<pre>";
-    print_r($tt);
-    echo "</pre>";
-
-    echo $tt['2021'] . '年';
+    echo $zodiac['2021'] . '年';
 
 ?>
 
