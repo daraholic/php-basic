e8795fd79a825ee28b32d1db52045b661ddc657b
annt <1112dara@gmail.com>
2021-11-10 16:32:47 +0800

    加了威力彩券註解

--- array/pra03.php
+++ array/pra03.php
@@ -13,12 +13,16 @@
 <?php
 
 // while 不固定次數
+// 設空陣列
 $nums=[];
 $i=0;
 while(count($nums)<6){
     $t=rand(1,38);
     if(!in_array($t,$nums)){
+        // 判斷跑出來的數字是不是有重複 沒有的話繼續跑
         $nums[]=$t;
+        // 這邊的while負責資料處理
+        // echo "<div class='ball'>".$t ."</div>";
     }
     $i++;
 }
@@ -29,10 +33,13 @@ while(count($nums)<6){
     echo "迴圈次數".$i;
     echo "<hr>";
 
-
+    // 印出上面while跑的數字(陣列)
+    // 這邊的foreach負責顯示
+    // 分開做事
     foreach($nums as $num){
         echo "<div class='ball'>".$num ."</div>";
     }
+    
     echo "<br>";
     echo "<div class='ball'>".rand(1,8)."</div>";
 
